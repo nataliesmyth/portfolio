28f7c974fa22f7afbf8cbad568a1a2ae979f1b69
Nataliesmyth <nataliesmyth11@gmail.com>
2023-12-31 13:20:36 -0500

    closed mobile nav on link click

--- scripts/nav.js
+++ scripts/nav.js
@@ -1,9 +1,10 @@
 console.log('sanity check')
 
-const nav = document.querySelector('.primary-navigation')
-const navToggle = document.querySelector('.mobile-nav-toggle')
-const closeNavBtn = document.querySelector('.close-nav-btn')
-console.log(nav)
+const nav = document.querySelector('.primary-navigation');
+const navToggle = document.querySelector('.mobile-nav-toggle');
+const closeNavBtn = document.querySelector('.close-nav-btn');
+let navLink = document.querySelectorAll('.nav-link');
+console.log(navLink)
 
 navToggle.addEventListener('click', () => { 
 
@@ -20,4 +21,11 @@ navToggle.addEventListener('click', () => {
         closeNavBtn.classList.add('hidden')
     }
     console.log(navToggle.getAttribute('aria-expanded'))
-})
+});
+
+for (let i = 0; i <= navLink.length; i++) {
+    navLink[i].addEventListener('click', () => {
+        nav.setAttribute('data-visible', false);
+    })
+}
+
